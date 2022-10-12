a021de312154326fb14c3dd1137ebf920dbeb7fb
OliverSoftwire <Oliver.Spence@softwire.com>
2022-10-12 10:54:07 +0100

    Initial FizzBuzz implementation

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,19 @@ function fizzbuzz() {
 	console.log('Welcome to FizzBuzz!');
 
 	for (let number = 1; number <= 100; number++) {
-		console.log(number);
+		output = "";
+		let shouldPrintNumber = true;
+
+		if (number % 3 == 0) {
+			output += "Fizz";
+			shouldPrintNumber = false;
+		}
+		if (number % 5 == 0) {
+			output += "Buzz";
+			shouldPrintNumber = false;
+		}
+
+		console.log(shouldPrintNumber ? number : output);
 	}
 }
 
