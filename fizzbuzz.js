2105df454d2aadab39144004fffe24eefe4952ce
OliverSoftwire <Oliver.Spence@softwire.com>
2022-10-12 10:48:19 +0100

    Cleanup formatting

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,7 @@
-
-
-// Here, we create our main function.
 function fizzbuzz() {
-    
-    console.log('Welcome to FizzBuzz!');
+	console.log('Welcome to FizzBuzz!');
 
-    // Put your code here...
-
-    
+	// Put your code here...
 }
 
-// Now we run the main function...
 fizzbuzz();
